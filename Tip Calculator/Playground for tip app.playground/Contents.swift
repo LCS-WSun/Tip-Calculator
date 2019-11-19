729fb2d4d88793f42ca38813cc7ab6462c10a3e6
Wesley Sun <wsun@lcs.on.ca>
2019-11-19 14:36:47 -0500

    Gordon showing how the logic of the app works

--- Tip Calculator/Playground for tip app.playground/Contents.swift
+++ Tip Calculator/Playground for tip app.playground/Contents.swift
@@ -1,6 +1,36 @@
 import UIKit
 
-let amountofBill = 1-100
-let tipPercentage = 1-20%
-let tip Percentage = 0.01-0.20
-let tip = 
+// 1. INPUT
+// Simulate text fields in the UI
+let billAmount = UITextField()
+let tipPercentage = UITextField()
+let splitBetweenHowManyPeople = UITextField()
+
+// Simulate user input
+billAmount.text = "100"
+tipPercentage.text = "15"
+splitBetweenHowManyPeople.text = "2"
+
+// Get actual strings from each text field
+// (force unwrapping the optional strings contained in each text field)
+let billAmountAsString = billAmount.text!
+let tipPercentageString = tipPercentage.text!
+let splitBetweenHowManyPeopleAsString = splitBetweenHowManyPeople.text!
+
+// Convert Strings to Double data type
+let billAmountAsDouble = Double(billAmountAsString)!
+let tipPercentageAsDouble = Double(tipPercentageString)! / 100
+let splitBetweenHowManyPeopleAsDouble = Double(splitBetweenHowManyPeopleAsString)!
+
+
+// 2. PROCESS
+let tipAmountInDollars = tipPercentageAsDouble * billAmountAsDouble
+
+
+
+
+
+//
+////Calculate the tip
+//let tipAmountInDollars = billAmountAsDouble * tipPercentageAs Double
+//
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Tip Calculator
 //
-//  Created by Sun, Wesley on 2019-10-10.
+//  Created by Sun, Wesley on 2019-11-19.
 //  Copyright © 2019 Wesley Sun. All rights reserved.
 //
 
@@ -12,20 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
-    //MARK: properties (outlets)
-    @outlet weak var :UIlabel!
-    
-    //MARK: Methods (actions)
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    
-    
+    */
 
 }
-
